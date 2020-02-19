e86f8329b70a8fb8067265b9538346471ec31794
AnandaDwiPrayoga <Prayogaananda04@gmail.com>
2020-02-19 10:22:13 +0700

    complete intent explicit

--- Intent/app/src/main/java/id/ac/polinema/intent/ExplicitIntentActivity.java
+++ Intent/app/src/main/java/id/ac/polinema/intent/ExplicitIntentActivity.java
@@ -3,16 +3,30 @@ package id.ac.polinema.intent;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.text.TextUtils;
 import android.view.View;
 import android.widget.EditText;
 import android.widget.TextView;
+import android.widget.Toast;
 
 public class ExplicitIntentActivity extends AppCompatActivity {
 
+    private EditText etName;
+    private TextView tvName;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_explicit_intent);
+
+        etName = findViewById(R.id.input_name);
+        tvName = findViewById(R.id.text_output);
     }
 
+    public void handleSubmit(View view) {
+        if (TextUtils.isEmpty(etName.getText())){
+            Toast.makeText(this, "Fill the blank", Toast.LENGTH_SHORT).show();
+        }else{
+            tvName.setText("Hello " + etName.getText().toString() + " , Congratulation!!");
+        }
+    }
 }
